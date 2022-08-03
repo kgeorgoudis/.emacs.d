aa45206f94e22f4c6d0eed471d61d6d1ce47398c
Konstantinos Georgoudis <k.georgoudis@pan-net.eu>
2022-08-03 23:55:22 +0300

    modify MELPA according to github daviwil/emacs-from-scratch

--- melpa.el
+++ melpa.el
@@ -1,9 +1,16 @@
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(package-initialize)
 
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+  ;; Initialize use-package on non-Linux platforms
 (unless (package-installed-p 'use-package)
-  (package-refresh-contents)
   (package-install 'use-package))
+
 (require 'use-package)
-(setq use-package-always-ensure 't)
+(setq use-package-always-ensure t)
