6587e69bf645bcb7cbb3d67715f43a0186111cd4
Konstantinos Georgoudis <k.georgoudis@pan-net.eu>
2022-08-04 17:15:25 +0300

    customize org-roam default capture template

--- org-roam.el
+++ org-roam.el
@@ -1,14 +1,31 @@
 (use-package org-roam
   :ensure t
+  :init
+  (setq org-roam-v2-ack t)
   :custom
   (org-roam-directory (file-truename "~/Notes/"))
+  (org-roam-completion-everywhere t)
+  (org-roam-capture-templates
+   '(("d" "default" plain
+      "Clusters: %?\nReferences: \n"
+      :if-new (file+head "${slug}.org" "#+title: ${title}\n#+created: %U\n")
+      :unnarrowed t)))
   :bind (("C-c n l" . org-roam-buffer-toggle)
          ("C-c n f" . org-roam-node-find)
          ("C-c n g" . org-roam-graph)
          ("C-c n i" . org-roam-node-insert)
+         ("C-c n I" . org-roam-node-insert-immediate)
          ("C-c n c" . org-roam-capture)
-         ;; Dailies
+         :map org-mode-map
          ("C-c n j" . org-roam-dailies-capture-today))
   :config
-  (org-roam-db-autosync-mode)
-  (require 'org-roam-protocol))
+  (org-roam-setup)
+  (org-roam-db-autosync-mode))
+
+
+(defun org-roam-node-insert-immediate (arg &rest args)
+  (interactive "P")
+  (let ((args (cons arg args))
+        (org-roam-capture-templates (list (append (car org-roam-capture-templates)
+                                                  '(:immediate-finish t)))))
+    (apply #'org-roam-node-insert args)))
