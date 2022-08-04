8ff13d0eeef323f75742df71d9b29c9aba4ac56f
Konstantinos Georgoudis <k.georgoudis@pan-net.eu>
2022-08-04 17:14:46 +0300

    startup improvements

--- general.el
+++ general.el
@@ -1,3 +1,14 @@
+;; The default is 800 kilobytes.  Measured in bytes.
+(setq gc-cons-threshold (* 50 1000 1000))
+;; Profile emacs startup
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (message "*** Emacs loaded in %s with %d garbage collections."
+                     (format "%.2f seconds"
+                             (float-time
+                              (time-subtract after-init-time before-init-time)))
+                     gcs-done)))
+(set-default-coding-systems 'utf-8)
 (setq inhibit-startup-message t
       visible-bell nil
       blink-cursor-mode nil
