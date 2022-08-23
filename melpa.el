e75f47cb96f920f0cc11b2af513b8eee54abbdae
Konstantinos Georgoudis <k.georgoudis@pan-net.eu>
2022-08-23 23:22:12 +0300

    add MELPA auto-update

--- melpa.el
+++ melpa.el
@@ -1,7 +1,6 @@
 (require 'package)
 
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")
                          ("elpa" . "https://elpa.gnu.org/packages/")))
 
 (package-initialize)
@@ -14,3 +13,14 @@
 
 (require 'use-package)
 (setq use-package-always-ensure t)
+(use-package auto-package-update
+  :custom
+  (auto-package-update-interval 7)
+  (auto-package-update-prompt-before-update t)
+  (auto-package-update-hide-results t)
+  :config
+  (auto-package-update-maybe)
+  (auto-package-update-at-time "09:00"))
+
+(use-package no-littering)
+l
