9c5563cbd90d64e008e610c221122d4c704d053e
Konstantinos Georgoudis <k.georgoudis@pan-net.eu>
2022-08-29 09:56:02 +0300

    spell check

--- flycheck.el
+++ flycheck.el
@@ -1,3 +1,12 @@
 (use-package flycheck
   :defer t
   :hook (lsp-mode . flycheck-mode))
+
+(use-package flyspell
+  :config
+  (setq ispell-program-name "/opt/homebrew/bin/aspell"
+        ispell-default-dictionary "en_GB")
+  :hook (text-mode . flyspell-mode)
+  :bind (("M-<f7>" . flyspell-buffer)
+         ("<f7>" . flyspell-word)
+         ("C-;" . flyspell-auto-correct-previous-word)))
