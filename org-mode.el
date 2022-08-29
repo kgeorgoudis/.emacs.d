6d93a78219421391106f893709d0ddba1100588b
Konstantinos Georgoudis <k.georgoudis@pan-net.eu>
2022-08-29 09:56:58 +0300

    enable org-superstar

--- org-mode.el
+++ org-mode.el
@@ -38,11 +38,17 @@
         org-edit-src-content-indentation 2
         org-hide-block-startup nil))
 
-(use-package org-bullets
-  :after org
-  :hook (org-mode . org-bullets-mode)
-  :custom
-  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+;;(use-package org-bullets
+;;  :after org
+;;  :hook (org-mode . org-bullets-mode)
+;;  :custom
+;;  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+(use-package org-superstar
+    :config
+    (setq org-superstar-special-todo-items t)
+    (add-hook 'org-mode-hook (lambda ()
+                               (org-superstar-mode 1))))
 
 (defun my/org-mode-visual-fill ()
   (setq visual-fill-column-width 140
